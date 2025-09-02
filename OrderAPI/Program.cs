4f5318419a201c7a2fb9501ffdeb7ff3f33029d5
huseyin-cangur <huseyincangur35@gmail.com>
2025-09-03 01:36:24 +0300

    Add database migration and seed initial data for Products and Customers

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using OrderAPI;
 using OrderAPI.AutoMapper;
+using OrderAPI.Entity;
 using OrderAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +25,34 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<OrderContext>();
+
+
+    db.Database.Migrate();
+
+    // Seed data
+    if (!db.Products.Any())
+    {
+        db.Products.AddRange(
+            new Product { Name = "Laptop" },
+            new Product { Name = "Telefon" },
+            new Product { Name = "Tablet" }
+        );
+
+        db.SaveChanges();
+    }
+    if (!db.Customers.Any())
+    {
+        db.Customers.AddRange(
+          new Customer { Name = "Hüseyin", Email = "huseyin@gmail.com", Address = "İzmir" },
+              new Customer { Name = "Ahmet", Email = "ahmet@gmail.com", Address = "İstanbul" }
+      );
+        db.SaveChanges();
+    }
 }
 
 app.UseHttpsRedirection();
